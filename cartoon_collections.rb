490dd5d38c074925109d41c99eb063bc8fd68930
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-07-27 19:06:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,5 +15,5 @@ end
 def find_the_cheese (snacks)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  snacks.include? {cheese_types}
+  snacks.include? if cheese_types
 end
