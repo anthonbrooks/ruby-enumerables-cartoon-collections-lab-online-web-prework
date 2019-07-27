2f27eb7fe6d3a3ae3cf275e8d9ea060ab5f8f7a6
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-07-27 19:09:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,6 @@ def long_planeteer_calls (calls)
 end
 
 def find_the_cheese (snacks)
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   snacks.find { |snack| cheese_types.include?(snack) }
 end
