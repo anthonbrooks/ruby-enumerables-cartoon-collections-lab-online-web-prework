8e9182b55131a0ab3df1a137df06027595a6847a
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-07-27 18:59:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-def summon_captain_planet {planeteer_calls}
+def summon_captain_planet (planeteer_calls)
   planeteer_calls.map! {|calls| calls.capitalize !}
 end
 
